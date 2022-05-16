7aa10f5876cfc23238bf2e168a3ba7d6304341cf
lonelyfloat <cephhead_prsnl@protonmail.com>
2022-05-15 20:12:02 -0400

    Added comments, improved test program

--- src/Main.hs
+++ src/Main.hs
@@ -8,57 +8,62 @@ import Data.Maybe
 
 -- Datatypes
 
+-- Parsing data of program
 data ParseData = ParseData {currentCommand :: Int, totalCommands :: Int, commands :: [StateT RaylangData IO ()], commandStrs :: [String]}
 
+-- List of values and location for program logic
 data RayData = RayData {
     vals::[Char], location :: Int
 }
 
+-- All the data used in the interpreter
 data RaylangData = RaylangData {rayData :: RayData, parseData :: ParseData}
-
-getClosestElemIndex :: Eq a => Int -> a -> [a] -> Int
-getClosestElemIndex ix x xs = (fromJust . elemIndex x) (drop ix xs)
 
 -- COMMANDS
 
+-- Increments the index of the currently accesed cell in the list vals.
 ptrIncrement :: Monad m => StateT RaylangData m ()
 ptrIncrement = do
     d <- get
     put d {rayData = (rayData d){location = location (rayData d) + 1}}
 
+-- Decrements the index of the currently accesed cell in the list vals.
 ptrDecrement :: Monad m => StateT RaylangData m ()
 ptrDecrement = do
     d <- get
     put d {rayData = (rayData d){location = location (rayData d) - 1}}
 
+-- Increments the value of the element at the index of the currently accesed cell in the list vals by 1.
 valIncrement :: Monad m => StateT RaylangData m ()
 valIncrement = do
     d <- get
     put d {rayData = (rayData d){vals = modifyVals (vals (rayData d)) (location (rayData d))}}
     where modifyVals c i = zipWith (\ a b -> if b == i then  chr (ord a + 1) else a) c [0..]
 
+-- Decrements the value of the element at the index of the currently accesed cell in the list vals by 1.
 valDecrement :: Monad m => StateT RaylangData m ()
 valDecrement = do
     d <- get
     put d {rayData = (rayData d){vals = modifyVals (vals (rayData d)) (location (rayData d))}}
     where modifyVals c i = zipWith (\ a b -> if b == i then  chr (ord a - 1) else a) c [0..]
 
-loopStart :: StateT RaylangData IO ()
+-- Starts a loop that will terminate when the value at the currently accessed position in the vals list is 0.
+loopStart :: Monad m => StateT RaylangData m ()
 loopStart = do
     d <- get
     let l = if (vals (rayData d) !! location (rayData d)) == chr 0 then getEndLoc d else currentCommand (parseData d)
-    -- liftIO $ print l
     put d {parseData = (parseData d){currentCommand = l}}
-    where getEndLoc k = currentCommand (parseData k) + getClosestElemIndex (currentCommand (parseData k)) "liblib" (commandStrs (parseData k))
+    where getEndLoc k = currentCommand (parseData k) + (fromJust . elemIndex "liblib") (drop (currentCommand (parseData k)) (commandStrs (parseData k)))
 
-loopEnd :: StateT RaylangData IO ()
+-- Ends a loop that will terminate when the value at the currently accessed position in the vals list is 0.
+loopEnd :: Monad m => StateT RaylangData m ()
 loopEnd = do
     d <- get
     let l = if (vals (rayData d) !! location (rayData d)) /= chr 0 then getBeginLoc d else currentCommand (parseData d)
-    --liftIO $ print l
     put d {parseData = (parseData d){currentCommand = l}}
     where getBeginLoc k = currentCommand (parseData k) - (fromJust . elemIndex "rayray") (reverse $ take (currentCommand (parseData k) + 1) (commandStrs (parseData k)))
 
+-- Gets an input char from the user and stores it at the currently accessed position in the vals list. 
 inputByte :: StateT RaylangData IO ()
 inputByte = do
     d <- get
@@ -66,31 +71,35 @@ inputByte = do
     put d {rayData= (rayData d){vals=modifyVals (vals (rayData d)) (location (rayData d)) c}}
     where modifyVals c i n = zipWith (\ a b -> if b == i then n else a) c [0..]
 
+-- Outputs a char at the currently accessed position in the vals list. 
 outputByte :: StateT RaylangData IO ()
 outputByte = do
     d <- get
-    --liftIO $ putChar (chr (ord (vals (rayData d) !! location (rayData d)) + 48))
     liftIO $ putChar (vals (rayData d) !! location (rayData d))
 
 -- Parsing, commands , etc.
 
+-- All commands in raylang.
 raylangCommands :: [String]
 raylangCommands = ["ray", "lib", "raylib", "libray", "rayray", "liblib", "libraylib", "raylibray"]
 
+-- All of the functions mapping to the commands.
 raylangFuncs :: [StateT RaylangData IO ()]
 raylangFuncs = [ptrIncrement, ptrDecrement, valIncrement, valDecrement, loopStart, loopEnd, inputByte, outputByte]
 
+-- Turns a command string into a raylang function.
 parse :: String -> StateT RaylangData IO ()
 parse str =  raylangFuncs !! fromJust (elemIndex str raylangCommands)
 
+-- Returns all of the valid commands in the source code.
 getValid :: String -> [String]
 getValid str = filter (`elem` raylangCommands) (words str)
 
--- toActions :: String -> [RayAction]
-
+-- Initial data created from source code.
 initialRayData :: String -> RaylangData
 initialRayData src = RaylangData {rayData = RayData {vals=repeat (chr 0), location=0},parseData = ParseData {commands = map parse (getValid src), commandStrs = getValid src, currentCommand = 0, totalCommands = length (getValid src)}}
 
+-- Executes all actons in RaylangData. 
 execute :: StateT RaylangData IO ()
 execute = do
     d <- get   
@@ -102,35 +111,22 @@ execute = do
         put dm {parseData = (parseData dm){currentCommand = l}} 
         execute
 
+-- Interprets a string into once I/O action.
 interpret :: String -> IO ()
 interpret str = evalStateT execute (initialRayData str)
 
--- main (with test program)getClosestElemIndex (10) "liblib" (test)
-
+-- Test program
 test :: String
-test = "raylib raylib raylib raylib raylibray \
-\ ray raylib raylib raylib raylib raylibray \
-\ rayray lib raylib ray libray liblib \
-\ raylib raylib raylib raylib raylib raylib raylib raylib \
-\ rayray \
-\ lib raylib raylib raylib raylib raylib raylib \
-\ ray libray \
-\ liblib lib raylibray"
-
-main :: IO ()
-main = interpret test
-    -- print $ 34 - (fromJust . elemIndex "rayray") (reverse $ take (34 + 1) (getValid test))
-
-
-
-{-
-
- "raylib raylib raylib raylibray                      sets first val to 3 \
+test =  "raylib raylib raylib raylibray                     sets first val to 3 \
 \ ray raylib raylib raylib raylib raylibray                 sets second val  to 4 \
-\ rayray lib raylib ray libray liblib                       adds the two, getting 7 \
+\ rayray lib raylib ray libray liblib                       adds the two values, getting 7 \
 \ raylib raylib raylib raylib raylib raylib raylib raylib   makes second val 8 \
 \ rayray \
 \ lib raylib raylib raylib raylib raylib raylib             adds 6 to first val \
-\ ray libray                                                subtracts 1 from second val \
+\ ray libray                                                subtracts 1 from second val until it is 0 \
 \ liblib lib raylibray                                      end loop" -- Adds two numbers
--}
+
+
+-- Main
+main :: IO ()
+main = interpret test
